15aa4932457aa629996efdd1e5a504f9fcbd0a09
Diego Moir <diegojmoir@gmail.com>
2018-04-06 00:11:25 -0600

    Attempt of generate the .exe

--- Analizador Lexico/src/analizador_lexico/AnalizadorLexico.java
+++ Analizador Lexico/src/analizador_lexico/AnalizadorLexico.java
@@ -18,9 +18,15 @@ public class AnalizadorLexico {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Interface().setVisible(true);
+            }
+        });
         String path = new File(".").getAbsolutePath();
         path = path.substring(0, path.length()-1) + "/src" + "/analizador_lexico" + "/PHP.flex";
         GenerateJflex(path);  
+       
     }
     
     public static void GenerateJflex(String path){
